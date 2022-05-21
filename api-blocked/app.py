3c944bdfadf779c2663f13b16c87e6d56b65b2d7
Buronn <fernando.buron@mail.udp.cl>
2022-05-21 18:04:07 -0400

    changes

--- api-blocked/app.py
+++ api-blocked/app.py
@@ -1,13 +1,14 @@
 from flask import Flask, jsonify, request
-
 from db.config import Config
+from threading import Event
+import signal
+from flask_kafka import FlaskKafka
 app = Flask(__name__)
 app.config.from_object(Config)
 
 @app.route('/blocked', methods=['GET'])
 def blocked():
-    print("blockeado cabron")
-    return jsonify({'blocked': True})
+    return jsonify({"blocked": True})
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=80, debug=True, threaded=True)
